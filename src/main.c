ec6fdff95b48f98352655ffd906af26cde44fc44
Salaah Amin <salaah01@users.noreply.github.com>
2024-03-09 18:45:12 +0000

    basic cli taking filename as arg

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello, World!");
-  return 0;
+int main(int argc, char *argv[])
+
+{
+  if (argc != 2)
+  {
+    printf("Invalid number of arguments\n");
+    printf("Usage: ./carp-editor <filename>\n");
+    return 1;
+  }
+
+  char *filename = argv[1];
 }
