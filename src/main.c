b875ff9f903620f683994a3166042a00205fcb0b
Salaah Amin <salaah01@users.noreply.github.com>
2024-03-09 19:13:27 +0000

    file opening

--- src/main.c
+++ src/main.c
@@ -5,4 +5,8 @@ int main(int argc, char *argv[])
 {
   char *filename = cli(argc, argv);
   printf("filename is %s\n", filename);
+
+  FILE* file_ptr;
+  file_ptr = fopen(filename, "a+");
+  fclose(file_ptr);
 }
