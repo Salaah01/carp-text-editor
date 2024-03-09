74544fe93da05f9f20cf7d1deb0937ae69401bfe
Salaah Amin <salaah01@users.noreply.github.com>
2024-03-09 22:29:26 +0000

    changes to raw mode on start

--- src/main.c
+++ src/main.c
@@ -1,12 +1,50 @@
+#include <ctype.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
 #include "cli.h"
+
+struct termios orig_termios;
+
+void disableRawMode()
+{
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode()
+{
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT| ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+  raw.c_cflag &= ~(CS8);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(int argc, char *argv[])
 {
+  enableRawMode();
   char *filename = cli(argc, argv);
-  printf("filename is %s\n", filename);
+  printf("filename is %s\r\n", filename);
 
-  FILE* file_ptr;
+  FILE *file_ptr;
   file_ptr = fopen(filename, "a+");
   fclose(file_ptr);
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+  {
+    if (iscntrl(c))
+    {
+      printf("%d\r\n", c);
+    }
+    else
+    {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  }
+  return 0;
 }
