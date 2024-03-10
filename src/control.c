8978b0cf661624592bf9f7cedbed28a4d220d474
Salaah Amin <salaah01@users.noreply.github.com>
2024-03-10 21:39:04 +0000

    page up/down function

--- src/control.c
+++ src/control.c
@@ -15,7 +15,9 @@ enum EditorKey
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN,
 };
 
 /**
@@ -43,6 +45,23 @@ int editorReadKey()
 
     if (seq[0] == '[')
     {
+
+      if (seq[1] >= '0' && seq[1] <= '9')
+      {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~')
+        {
+          switch (seq[1])
+          {
+          case '5':
+            return PAGE_UP;
+          case '6':
+            return PAGE_DOWN;
+          }
+        }
+      }
+
       switch (seq[1])
       {
       case 'A':
@@ -111,6 +130,17 @@ void editorProcessKeypress()
     write(STDERR_FILENO, "\x1b[H", 3);
     exit(0);
     break;
+
+  case PAGE_UP:
+  case PAGE_DOWN:
+  {
+    int times = editorConfig.screen_rows;
+    while (times--)
+    {
+      editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+  }
+  break;
 
   case ARROW_UP:
   case ARROW_LEFT:
